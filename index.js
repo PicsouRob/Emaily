eddc9cf264726c8b38d54c9231ab411738f2e9c1
Phanord Roberto <phanordpicsouroberto11@gmail.com>
2021-11-29 23:18:39 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send({ message: 'Running' });
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log('server running'));
